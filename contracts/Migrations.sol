50eb5b1be73c3912031deefc0104befbc3b67951
hyeongwoo.lim <lkslyj2@naver.com>
2022-02-02 21:39:36 +0900

    add: migration code for Truffle

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.4.15;
+
 contract Migrations {
   address public owner = msg.sender;
   uint public last_completed_migration;
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
-const Migrations = artifacts.require("Migrations");
+var fs = require("fs");
+var Lottery = artifacts.require("Lottery");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = function (deployer, network) {
+  // unlock account for geth
+  if (network == "rinkeby" || network == "mainnet") {
+    var password = fs.readFileSync("password", "utf8").split("\n")[0];
+    web3.personal.unlockAccount(web3.eth.accounts[0], password);
+  }
+
+  var duration = 3600 * 24 * 3; // 3 days
+  deployer.deploy(Lottery, duration);
 };
