264d99f02d963545883d568d4855f5c5920f5b8f
hyeongwoo.lim <lkslyj2@naver.com>
2022-02-03 18:40:22 +0900

    refactor: initial migration back to initial state and new migration file is created

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,18 +1,5 @@
-var fs = require("fs");
-var Lottery = artifacts.require("Lottery");
+const Migrations = artifacts.require("Migrations");
 
-module.exports = function (deployer, network) {
-  var duration = 3600 * 1 * 1; // seconds * hour * day
-  var contractInstance;
-
-  deployer.deploy(Lottery, duration);
-  Lottery.deployed().then((instance) => (contractInstance = instance));
-
-  web3.eth.accounts[i].forEach((address) => {
-    // unlock account for geth
-    if (network == "rinkeby" || network == "mainnet") {
-      var password = fs.readFileSync("password", "utf8").split("\n")[i];
-      web3.personal.unlockAccount(web3.eth.accounts[i], password);
-    }
-  });
+module.exports = function (deployer) {
+  deployer.deploy(Migrations);
 };
