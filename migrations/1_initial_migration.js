808925d4f9075f2e2d31e9464f09155549cbd158
hyeongwoo.lim <lkslyj2@naver.com>
2022-02-03 18:36:18 +0900

    feature: migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,12 +2,17 @@ var fs = require("fs");
 var Lottery = artifacts.require("Lottery");
 
 module.exports = function (deployer, network) {
-  // unlock account for geth
-  if (network == "rinkeby" || network == "mainnet") {
-    var password = fs.readFileSync("password", "utf8").split("\n")[0];
-    web3.personal.unlockAccount(web3.eth.accounts[0], password);
-  }
+  var duration = 3600 * 1 * 1; // seconds * hour * day
+  var contractInstance;
 
-  var duration = 3600 * 1 * 1; // 3 days
   deployer.deploy(Lottery, duration);
+  Lottery.deployed().then((instance) => (contractInstance = instance));
+
+  web3.eth.accounts[i].forEach((address) => {
+    // unlock account for geth
+    if (network == "rinkeby" || network == "mainnet") {
+      var password = fs.readFileSync("password", "utf8").split("\n")[i];
+      web3.personal.unlockAccount(web3.eth.accounts[i], password);
+    }
+  });
 };
