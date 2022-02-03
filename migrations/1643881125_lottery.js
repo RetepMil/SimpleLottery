132d2113ea23730c96a6fb1bff8e2e870665dc23
hyeongwoo.lim <lkslyj2@naver.com>
2022-02-03 18:56:39 +0900

    refactor: lottery contract migration code to use .then structure to print accounts in console.

--- migrations/1643881125_lottery.js
+++ migrations/1643881125_lottery.js
@@ -8,11 +8,19 @@ module.exports = function (deployer, network) {
   deployer.deploy(Lottery, duration);
   Lottery.deployed().then((instance) => (contractInstance = instance));
 
-  web3.eth.accounts[i].forEach((address) => {
-    // unlock account for geth
-    if (network == "rinkeby" || network == "mainnet") {
-      var password = fs.readFileSync("password", "utf8").split("\n")[i];
-      web3.personal.unlockAccount(web3.eth.accounts[i], password);
-    }
-  });
+  web3.eth
+    .getAccounts()
+    .then((accounts) => {
+      console.log(accounts);
+      return accounts;
+    })
+    .then((accounts) =>
+      accounts.forEach((address) => {
+        // unlock account for geth
+        if (network == "rinkeby" || network == "mainnet") {
+          var password = fs.readFileSync("password", "utf8").split("\n")[i];
+          web3.personal.unlockAccount(address, password);
+        }
+      })
+    );
 };
